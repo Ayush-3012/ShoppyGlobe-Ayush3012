7c627310d4d4ae353ec9fbbdff8385768b9e8337
Ayush-3012 <diveshsrivastava31@gmail.com>
2025-02-21 15:42:45 +0530

    setting up the project

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <SnackbarProvider
+      maxSnack={2}
+      autoHideDuration={2000}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "center",
+      }}
+    >
+      <App />
+    </SnackbarProvider>
+  </BrowserRouter>
+);
