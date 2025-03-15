ffc29b8e0622845bf3746fe6e2eada3f636a00a7
Ayush-3012 <diveshsrivastava31@gmail.com>
2025-03-15 19:15:23 +0530

    styled the details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -43,6 +43,23 @@ const ProductDetails = () => {
     fetchDetails();
   }, [productId]);
 
+  const dateFormatter = (timestamp) => {
+    const myDate = new Date(timestamp);
+
+    const options = {
+      weekday: "short",
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone: "UTC",
+    };
+
+    return myDate.toLocaleString("en-GB", options).replace(",", "");
+  };
+
   if (loading) {
     return <Loader />;
   }
@@ -71,7 +88,7 @@ const ProductDetails = () => {
       </div>
     );
   }
-
+  console.log(productDetails);
   return (
     <>
       <motion.div
@@ -86,12 +103,11 @@ const ProductDetails = () => {
               {productDetails.title}
             </h1>
             <p className="text-gray-200 py-1 mx-4 text-xl">
-              {productDetails.category}
+              {productDetails.category.toUpperCase()}
             </p>
           </div>
           {/* Buy Now Button */}
-          <motion.div className="flex justify-center"
-          >
+          <motion.div className="flex justify-center">
             <button
               className="bg-green-500 text-2xl cursor-pointer hover:scale-x-110 hover:bg-green-600 text-white px-5 py-2 rounded-lg font-semibold transition"
               onClick={() => updateMyCart(productDetails)}
@@ -106,7 +122,7 @@ const ProductDetails = () => {
           <img
             src={currentImage || productDetails.thumbnail}
             alt={productDetails.title}
-            className="rounded-xl shadow-green-400 bg-slate-300 shadow-[1px_1px_10px] w-full md:w-1/3"
+            className="rounded-xl shadow-green-400 w-96 h-96 bg-slate-600 shadow-[1px_1px_10px]  "
           />
           <div className="flex flex-wrap gap-5">
             {productDetails.images.map((img, index) => (
@@ -114,7 +130,7 @@ const ProductDetails = () => {
                 key={index}
                 src={img}
                 alt={`Image ${index}`}
-                className="w-40 h-40 cursor-pointer rounded-lg hover:shadow-green-500  shadow-[1px_1px_10px]"
+                className="w-40 h-48 cursor-pointer rounded-lg hover:shadow-green-500  shadow-[1px_1px_10px]"
                 onClick={(e) => setCurrentImage(e.target.src)}
                 whileHover={{ translateY: "-10px" }}
               />
@@ -123,28 +139,55 @@ const ProductDetails = () => {
         </div>
 
         {/* Price & Stock */}
-        <div className="mt-4 flex justify-between items-center">
-          <p className="text-2xl font-bold text-green-300">
-            ${productDetails.price}
-          </p>
-          <p
-            className={`text-lg font-semibold ${
-              productDetails.stock > 0 ? "text-green-400" : "text-red-400"
-            }`}
-          >
-            {productDetails.availabilityStatus}
-          </p>
+        <div className="mt-4 px-2 flex justify-between items-center">
+          <div>
+            <p className="text-2xl font-bold text-green-300">
+              Price: ${productDetails.price}
+            </p>
+            <p className="text-2xl font-bold text-red-300">
+              Discount: -{productDetails.discountPercentage}% off
+            </p>
+          </div>
+          {productDetails.tags.length > 0 && (
+            <div className=" text-2xl flex items-center justify-center gap-2">
+              Tags:
+              <div className="flex gap-3 flex-wrap items-center justify-between">
+                {productDetails.tags.map((tag, index) => (
+                  <p
+                    key={index}
+                    className="px-2 rounded-xl text-lg bg-green-400 text-slate-800 font-bold font-mono hover:-translate-y-1.5 hover:shadow-white hover:shadow-[1px_1px_10px] transition duration-150"
+                  >
+                    {tag.toUpperCase()}
+                  </p>
+                ))}
+              </div>
+            </div>
+          )}
+          <div>
+            <p className="text-2xl font-bold text-yellow-400">
+              Rating: ⭐{productDetails.rating}/5
+            </p>
+            <p
+              className={`text-lg font-semibold ${
+                productDetails.stock > 0 ? "text-green-400" : "text-red-400"
+              }`}
+            >
+              {productDetails.availabilityStatus}
+            </p>
+          </div>
         </div>
 
         {/* Description */}
-        <p className="mt-3 text-gray-300">{productDetails.description}</p>
+        <p className="mt-5 text-xl text-gray-300">
+          {productDetails.description}
+        </p>
 
         {/* Details Table */}
-        <div className="mt-5 bg-gray-800 p-4 rounded-lg">
-          <h2 className="text-xl font-semibold mb-2 text-green-300">
+        <div className="mt-5">
+          <h2 className="text-2xl font-semibold text-green-300">
             Product Details
           </h2>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid bg-gray-800 p-4 rounded-lg grid-cols-2 gap-4">
             <p>
               <span className="font-semibold">Brand:</span>{" "}
               {productDetails.brand}
@@ -173,22 +216,32 @@ const ProductDetails = () => {
 
         {/* Customer Reviews */}
         <div className="mt-5">
-          <h2 className="text-xl font-semibold text-green-300">
+          <h2 className="text-2xl font-semibold text-green-300">
             Customer Reviews
           </h2>
-          {productDetails?.reviews?.length > 0 ? (
-            productDetails?.reviews?.map((review, index) => (
-              <div key={index} className="bg-gray-800 p-3 mt-3 rounded-lg">
-                <p className="text-lg text-yellow-400">
-                  ⭐ {review.rating} / 5
-                </p>
-                <p className="italic">&quot;{review.comment}&quot;</p>
-                <p className="text-sm text-gray-400">- {review.reviewerName}</p>
-              </div>
-            ))
-          ) : (
-            <p className="text-gray-400 mt-2">No reviews yet.</p>
-          )}
+          <div className="bg-gray-800 rounded-lg px-4 flex flex-col gap-2 py-2">
+            {productDetails?.reviews?.length > 0 ? (
+              productDetails?.reviews?.map((review, index) => (
+                <div
+                  key={index}
+                  className="p-3 flex justify-between items-center bg-slate-950 rounded-lg hover:shadow-[1px_1px_10px] shadow-green-400 duration-150"
+                >
+                  <div>
+                    <p className="text-lg text-yellow-400">
+                      ⭐ {review.rating} / 5
+                    </p>
+                    <p className="italic">&quot;{review.comment}&quot;</p>
+                    <p className="text-sm text-gray-400">
+                      - {review.reviewerName}
+                    </p>
+                  </div>
+                  <p className="italic">{dateFormatter(review.date)}</p>
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-400 mt-2">No reviews yet.</p>
+            )}
+          </div>
         </div>
       </motion.div>
     </>
